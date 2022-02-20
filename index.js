926fe168f322ca6c49c799cedd799d4303e65626
nichoth <nichoth@gmail.com>
2022-02-20 14:35:06 -0800

    server side works

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-// const { initializeApp } = require('firebase-admin/app')
+// this is a server side file
 
+const Firestore = require('@google-cloud/firestore')
 
-var admin = require("firebase-admin")
-var serviceAccount = require("./test-nov-10-firebase-adminsdk-1cmdm-5ec062ea7c.json")
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://test-nov-10.firebaseio.com"
+const db = new Firestore({
+    projectId: 'test-nov-10',
+    keyFilename: __dirname + '/test-nov-10-firebase-adminsdk-1cmdm-5ec062ea7c.json'
 })
 
+const docRef = db.collection('users').doc('alovelace')
 
+docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+})
+.then(res => {
+    console.log('res', res)
+})
+.catch(err => {
+    console.log('errrrrr', err)
+})
